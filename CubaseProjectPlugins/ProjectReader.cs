6351a802889282dcfa5e457eca1cdaaa86e313aa
Fotis Gimian <fgimiansoftware@gmail.com>
2021-11-13 16:02:00 +1100

    Improve error handling when parsing project files.

--- CubaseProjectPlugins/ProjectReader.cs
+++ CubaseProjectPlugins/ProjectReader.cs
@@ -48,73 +48,146 @@ public class ProjectReader
         for (int i = 0; i < _projectBytes.Length; i++)
         {
             // Check if the current byte matches the first byte we're searching for.
-            if (_projectBytes[i] != pluginUidSearchTerm[0])
+            if (_projectBytes[i] != (char)'P')
             {
-                continue;
-            }
-
-            // Check that the next set of bytes relate to a plugin.
-            byte[] uidTerm = _projectBytes[i..(i + pluginUidSearchTerm.Length)];
-            if (uidTerm.SequenceEqual(pluginUidSearchTerm))
-            {
-                _index = i + pluginUidSearchTerm.Length + 22;
-                string key;
-                string name;
-
-                _ = GetToken(); // GUID
-                _index += 3;
-
-                key = GetToken();
-                if (key != "Plugin Name")
-                {
-                    continue;
-                }
-
-                _index += 5;
-                name = GetToken();
-
-                _index += 3;
-                key = GetToken();
-
-                if (key == "Original Plugin Name")
-                {
-                    _index += 5;
-                    name = GetToken();
-                }
-
-                // Skip names that are to be ignored.
-                if (IgnoreNames.Contains(name))
-                {
-                    continue;
-                }
-
-                plugins.Add(name);
                 continue;
             }
 
             // Check that the next set of bytes related to the Cubase version.
-            byte[] versionTerm = _projectBytes[i..(i + appVersionSearchTerm.Length)];
-            if (versionTerm.SequenceEqual(appVersionSearchTerm))
+            try
             {
-                _index = i + appVersionSearchTerm.Length + 9;
-                cubaseApplication = GetToken();
-
-                _index += 3;
-                cubaseVersion = GetToken();
-
-                _index += 3;
-                cubaseReleaseDate = GetToken();
-
-                _index += 7;
-                try
+                byte[] versionTerm = _projectBytes[i..(i + appVersionSearchTerm.Length)];
+                if (versionTerm.SequenceEqual(appVersionSearchTerm))
                 {
-                    architecture = GetToken();
+                    _index = i + appVersionSearchTerm.Length + 9;
+                    try
+                    {
+                        cubaseApplication = GetToken();
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        throw new InvalidDataException("Unable to obtain the app name");
+                    }
+
+                    _index += 3;
+                    try
+                    {
+                        cubaseVersion = GetToken();
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        throw new InvalidDataException("Unable to obtain app version");
+                    }
+
+                    _index += 3;
+                    try
+                    {
+                        cubaseReleaseDate = GetToken();
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        throw new InvalidDataException("Unable to obtain the app release date");
+                    }
+
+                    _index += 7;
+                    try
+                    {
+                        architecture = GetToken();
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        // Older 32-bit versions of Cubase didn't list the architecture in the project file.
+                        architecture = "Not Specified";
+                    }
                 }
-                catch (IndexOutOfRangeException)
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                // Ignore situations where we've gone beyond the end of the file.
+            }
+
+            // Check that the next set of bytes relate to a plugin.
+            try
+            {
+                byte[] uidTerm = _projectBytes[i..(i + pluginUidSearchTerm.Length)];
+                if (uidTerm.SequenceEqual(pluginUidSearchTerm))
                 {
-                    // Older 32-bit versions of Cubase didn't list the architecture in the project file.
-                    architecture = "WIN32";
+                    _index = i + pluginUidSearchTerm.Length + 22;
+
+                    string key;
+                    string name;
+
+                    try
+                    {
+                        _ = GetToken(); // GUID
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        throw new InvalidDataException("Unable to obtain the plugin GUID");
+                    }
+
+                    _index += 3;
+                    try
+                    {
+                        key = GetToken();
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        throw new InvalidDataException("Unable to obtain a required key token");
+                    }
+
+                    if (key != "Plugin Name")
+                    {
+                        continue;
+                    }
+
+                    _index += 5;
+                    try
+                    {
+                        name = GetToken();
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        throw new InvalidDataException("Unable to obtain the plugin name");
+                    }
+
+                    _index += 3;
+                    try
+                    {
+                        key = GetToken();
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        throw new InvalidDataException("Unable to obtain a required key token");
+                    }
+
+                    if (key == "Original Plugin Name")
+                    {
+                        _index += 5;
+                        try
+                        {
+                            name = GetToken();
+                        }
+                        catch (IndexOutOfRangeException)
+                        {
+                            throw new InvalidDataException(
+                                "Unable to obtain the original plugin name");
+                        }
+                    }
+
+                    // Skip names that are to be ignored.
+                    if (IgnoreNames.Contains(name))
+                    {
+                        continue;
+                    }
+
+                    plugins.Add(name);
+                    continue;
                 }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                // Ignore situations where we've gone beyond the end of the file.
             }
         }
 
