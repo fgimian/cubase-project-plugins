16e2046dff46c954cb37a931d23deb2bd2245e47
Fotis Gimian <fgimiansoftware@gmail.com>
2022-09-15 21:38:56 +1000

    Implement error handling for the project reader.

--- src/models/project.rs
+++ src/models/project.rs
@@ -1,8 +1,24 @@
 use std::collections::HashSet;
 
+/// Contains information about the Cubase version used to create the project
+#[derive(Debug, Eq, PartialEq, Hash)]
+pub struct ProjectMetadata {
+    /// The Cubase application name.
+    pub application: String,
+
+    /// The Cubase version used to create the project.
+    pub version: String,
+
+    /// The release date of the Cubase version used.
+    pub release_date: String,
+
+    /// The system architecture of the Cubase version used.
+    pub architecture: String,
+}
+
 /// Represents a plugin within a Cubase project.
 #[derive(Debug, Eq, PartialEq, Hash)]
-pub struct Plugin {
+pub struct ProjectPlugin {
     /// The globally unique identifier for the plugin.
     pub guid: String,
 
@@ -13,18 +29,9 @@ pub struct Plugin {
 /// Captures the Cubase version and all plugins used for a Cubase project.
 #[derive(Debug)]
 pub struct ProjectDetails {
-    /// The Cubase application name.
-    pub cubase_application: String,
-
-    /// The Cubase version used to create the project.
-    pub cubase_version: String,
-
-    /// The release date of the Cubase version used.
-    pub cubase_release_date: String,
-
-    /// The system architecture of the Cubase version used.
-    pub architecture: String,
+    /// Information about the Cubase version used to create the project.
+    pub metadata: ProjectMetadata,
 
     /// All the plugins used in the project.
-    pub plugins: HashSet<Plugin>,
+    pub plugins: HashSet<ProjectPlugin>,
 }
--- src/reader.rs
+++ src/reader.rs
@@ -1,7 +1,6 @@
 use crate::cstring_extras;
+use crate::models::project::{ProjectDetails, ProjectMetadata, ProjectPlugin};
 use std::collections::HashSet;
-
-use crate::models::project::{Plugin, ProjectDetails};
 
 const PLUGIN_UID_SEARCH_TERM: &[u8] = b"Plugin UID\0";
 const APP_VERSION_SEARCH_TERM: &[u8] = b"PAppVersion\0";
@@ -38,102 +37,130 @@ impl Reader {
     /// Obtains all project details including Cubase version and plugins used.
     // /// <returns>An instance of <see cref="ProjectDetails"/>containing project details.</returns>
     pub fn get_project_details(&self) -> ProjectDetails {
+        let mut metadata = ProjectMetadata {
+            application: String::from("Cubase"),
+            version: String::from("Unknown"),
+            release_date: String::from("Unknown"),
+            architecture: String::from("Unknown"),
+        };
         let mut plugins = HashSet::new();
-        let mut cubase_application = String::from("Cubase");
-        let mut cubase_version = String::from("Unknown");
-        let mut cubase_release_date = String::from("Unknown");
-        let mut architecture = String::from("Unknown");
 
         for (index, &byte) in self.project_bytes.iter().enumerate() {
             if char::from(byte) != 'P' {
                 continue;
             }
 
-            let mut current_index: usize;
-
             // Check that the next set of bytes related to the Cubase version.
-            current_index = index;
-            let version_term =
-                self.get_token_bytes_with_len(&mut current_index, APP_VERSION_SEARCH_TERM.len());
-            if version_term == APP_VERSION_SEARCH_TERM {
-                current_index += APP_VERSION_SEARCH_TERM.len() + 9;
-                cubase_application = self.get_token(&mut current_index);
-                current_index += 3;
-                cubase_version = self.get_token(&mut current_index);
-                current_index += 3;
-                cubase_release_date = self.get_token(&mut current_index);
-                current_index += 7;
-                architecture = self.get_token(&mut current_index);
-                // Older 32-bit versions of Cubase didn't list the architecture in the project file.
-                // architecture = "Not Specified";
-                continue;
+            if let Some(found_metadata) = self.search_metadata(index) {
+                metadata = found_metadata;
             }
 
             // Check that the next set of bytes relate to a plugin.
-            current_index = index;
-            let uid_term =
-                self.get_token_bytes_with_len(&mut current_index, PLUGIN_UID_SEARCH_TERM.len());
-            if uid_term == PLUGIN_UID_SEARCH_TERM {
-                current_index += PLUGIN_UID_SEARCH_TERM.len() + 22;
-                let guid = self.get_token(&mut current_index);
-                current_index += 3;
-
-                let mut key: String;
-
-                key = self.get_token(&mut current_index);
-                if key != "Plugin Name" {
-                    continue;
-                }
-                current_index += 5;
-
-                let mut name = self.get_token(&mut current_index);
-                current_index += 3;
-                key = self.get_token(&mut current_index);
-                if key == "Original Plugin Name" {
-                    current_index += 5;
-                    name = self.get_token(&mut current_index);
-                }
-
+            if let Some(found_plugin) = self.search_plugin(index) {
                 // Skip GUIDs that are to be ignored.
-                if self.guid_ignores.contains(&guid) {
+                if self.guid_ignores.contains(&found_plugin.guid) {
                     continue;
                 }
 
                 // Skip names that are to be ignored.
-                if self.name_ignores.contains(&name) {
+                if self.name_ignores.contains(&found_plugin.name) {
                     continue;
                 }
 
-                let plugin = Plugin { guid, name };
-                plugins.insert(plugin);
+                plugins.insert(found_plugin);
             }
         }
 
-        ProjectDetails {
-            cubase_application,
-            cubase_version,
-            cubase_release_date,
-            architecture,
-            plugins,
+        ProjectDetails { metadata, plugins }
+    }
+
+    fn search_metadata(&self, index: usize) -> Option<ProjectMetadata> {
+        let mut current_index = index;
+
+        let version_term =
+            self.get_token_bytes_with_len(&mut current_index, APP_VERSION_SEARCH_TERM.len())?;
+
+        if version_term != APP_VERSION_SEARCH_TERM {
+            return None;
         }
+
+        current_index += APP_VERSION_SEARCH_TERM.len() + 9;
+        let application = self.get_token(&mut current_index)?;
+        current_index += 3;
+        let version = self.get_token(&mut current_index)?;
+        current_index += 3;
+        let release_date = self.get_token(&mut current_index)?;
+        current_index += 7;
+        // Older 32-bit versions of Cubase didn't list the architecture in the project file.
+        let architecture = match self.get_token(&mut current_index) {
+            Some(architecture) => architecture,
+            None => String::from("Not Specified"),
+        };
+
+        Some(ProjectMetadata {
+            application,
+            version,
+            release_date,
+            architecture,
+        })
     }
 
-    fn get_token_bytes_with_len(&self, current_index: &mut usize, len: usize) -> Vec<u8> {
+    fn search_plugin(&self, index: usize) -> Option<ProjectPlugin> {
+        let mut current_index = index;
+
+        let uid_term =
+            self.get_token_bytes_with_len(&mut current_index, PLUGIN_UID_SEARCH_TERM.len())?;
+
+        if uid_term != PLUGIN_UID_SEARCH_TERM {
+            return None;
+        }
+
+        current_index += PLUGIN_UID_SEARCH_TERM.len() + 22;
+        let guid = self.get_token(&mut current_index)?;
+        current_index += 3;
+
+        let mut key: String;
+
+        key = self.get_token(&mut current_index)?;
+        if key != "Plugin Name" {
+            return None;
+        }
+        current_index += 5;
+
+        let mut name = self.get_token(&mut current_index)?;
+        current_index += 3;
+        key = self.get_token(&mut current_index)?;
+        if key == "Original Plugin Name" {
+            current_index += 5;
+            name = match self.get_token(&mut current_index) {
+                Some(original_name) => original_name,
+                None => name,
+            };
+        }
+
+        Some(ProjectPlugin { guid, name })
+    }
+
+    fn get_token_bytes_with_len(&self, current_index: &mut usize, len: usize) -> Option<Vec<u8>> {
         let end = *current_index + len;
-        let buffer = &self.project_bytes[*current_index..end];
-        buffer.to_vec()
+        if end > self.project_bytes.len() {
+            return None;
+        }
+
+        let token_bytes = &self.project_bytes[*current_index..end];
+        Some(token_bytes.to_vec())
     }
 
-    fn get_token(&self, current_index: &mut usize) -> String {
+    fn get_token(&self, current_index: &mut usize) -> Option<String> {
         let len = usize::from(self.project_bytes[*current_index]);
         *current_index += 1;
 
-        let token_bytes = self.get_token_bytes_with_len(current_index, len);
+        let token_bytes = self.get_token_bytes_with_len(current_index, len)?;
         *current_index += len;
 
         cstring_extras::from_vec_until_nul(token_bytes.to_vec())
-            .unwrap()
+            .ok()?
             .into_string()
-            .unwrap()
+            .ok()
     }
 }
