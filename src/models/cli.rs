16b9c0b4a6077e7d2d000a7dfaf3eb68dc577b48
Fotis Gimian <fgimiansoftware@gmail.com>
2023-03-18 13:39:08 +1100

    Update all dependencies to their latest versions.

--- src/models/cli.rs
+++ src/models/cli.rs
@@ -4,13 +4,13 @@ use clap::Parser;
 
 /// Obtains information and download links for Native Instruments products
 #[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
+#[command(author, version, about, long_about = None)]
 pub struct Cli {
     /// Set the directory paths to search for Cubase projects.
-    #[clap(value_name = "PROJECT_PATH")]
+    #[arg(value_name = "PROJECT_PATH")]
     pub project_paths: Vec<String>,
 
     /// Set the config file path.
-    #[clap(short, long, value_name = "PATH")]
+    #[arg(short, long, value_name = "PATH")]
     pub config: Option<PathBuf>,
 }
